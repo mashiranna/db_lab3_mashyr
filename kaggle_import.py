8dceb96e82fd183eaf1190abcaf32dab059b3138
mashiranna <55777589+mashiranna@users.noreply.github.com>
2021-12-29 18:24:11 +0200

    Add files via upload

--- kaggle_import.py
+++ kaggle_import.py
@@ -4,43 +4,39 @@ import psycopg2
 con = psycopg2.connect(user='mashyr_anna', password='1234', dbname='mashyr_anna_DB', host='localhost', port='5432')
 print(type(con))
 
-INPUT_CSV_FILE = 'app.csv'
+INPUT_CSV = 'windows_store.csv'
 
-query_1 = '''
-CREATE TABLE app_new
+query_0 = '''
+CREATE TABLE app_new_new
 ( 
-    app_name     character varying(60),
-    publisher    character varying(50),
-    price        character varying(50),
-    description  character varying(1600),
-    app_size     character varying(50),
-    category     character varying(30),
-    age_rating   character varying(30),
-    CONSTRAINT app_name PRIMARY KEY (app_name)
+    Name     character varying(1000) UNIQUE NOT NULL,
+    Publisher    character varying(1000) NOT NULL,
+    Price        character varying(100) NOT NULL,
+    PRIMARY KEY (Name)
 )
 '''
 
-query_2 = '''
-DELETE FROM app_new
+query_1 = '''
+DELETE FROM app_new_new
 '''
 
-query_3 = '''
-INSERT INTO app_new (app_name, publisher, price, description, app_size, category, age_rating ) VALUES (%s, %s, %s, %s, %s, %s ,%s)
+query_2 = '''
+INSERT INTO app_new_new(Name, Publisher, Price) VALUES (%s, %s, %s)
 '''
 
 
 with con:
-
     cur = con.cursor()
-    cur.execute('drop  table if exists app_new')
+    cur.execute('drop table if exists app_new_new')
+    cur.execute(query_0)
     cur.execute(query_1)
-    cur.execute(query_2)
 
-    with open(INPUT_CSV_FILE, 'r') as file:
-        reader = csv.DictReader(file)
+    with open(INPUT_CSV, 'r', encoding="utf8") as inf:
+        reader = csv.DictReader(inf)
 
         for idx, row in enumerate(reader):
-            values = (idx, row['Name'], row['Publisher'])
+            values = (row['Name'], row['Publisher'], row['Price'])
             cur.execute(query_2, values)
 
     con.commit()
+
